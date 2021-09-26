85faf159f4907e1c192d665fcf59ce6620661fe4
Paulina Podraza <paulina.podraza92@gmail.com>
2021-09-26 16:07:03 +0200

    Corect the code

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -7,25 +7,31 @@ import Creator from '../Creator/Creator';
 import Icon from '../Icon/Icon';
 
 class Column extends React.Component {
+  state = {
+  cards: this.props.cards || [],
+  }
   static propTypes = {
     title: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired,
+    cards: PropTypes.array,
   }
 
   addCard(title){
-  this.setState(state => (
-    {
-      cards: [
-        ...state.cards,
-        {
-          key: state.cards.length ? state.cards[state.cards.length-1].key+1 : 0,
-          title,
-        }
-      ]
-    }
-  ));
+    this.setState(state => (
+      {
+        cards: [
+          ...state.cards,
+          {
+            key: state.cards.length ? state.cards[state.cards.length-1].key+1 : 0,
+            title,
+          }
+        ]
+      }
+    ));
   }
 
   render() {
+    console.log(this.props);
     return (
     <section className={styles.component}>
         <h3 className={styles.title}>
@@ -39,6 +45,11 @@ class Column extends React.Component {
       </div>
       <div className={styles.creator}>
           <Creator text={settings.cardCreatorText} action={title => this.addCard(title)}/>
+        </div>
+                <div className={styles.cards}>
+        {this.state.cards.map(({ key, ...cardProps }) => (
+        <Card key={key} {...cardProps} />
+        ))}
         </div>
     </section>
     )
